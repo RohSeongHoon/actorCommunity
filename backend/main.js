f50b1bf9176fce0f57c358e7a97c2f98d9dcef9d
TTeongHoon <87319344+DDeunghoon@users.noreply.github.com>
2021-11-22 16:46:20 +0900

    데이터베이스 변경 api도 같이 변경해줌

--- backend/main.js
+++ backend/main.js
@@ -28,7 +28,12 @@ const mainRouter = express.Router();
   });
   mainRouter.get("/mainPost/:contents", (req, res) => {
     let contents = req.params.contents;
-    let query = 'select * from '+contents+' limit 4;' ;
+    if(contents == "media_freepost"){contents = 1}
+    if(contents == "theater_freepost"){contents = 2}
+    if(contents == "musical_freepost"){contents = 3}
+    if(contents == "dancer_freepost"){contents = 4}
+    let query = 'select title from board where category_number ='+contents+' limit 6'  ;
+    //댓글수는 어떻게 넣지 
     model.query(query,function(err,result){
       if(err){
         console.log(err);
