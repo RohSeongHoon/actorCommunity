5cdfeaabed9b1a094da533c6fab568e81d7bcf03
TTeongHoon <87319344+DDeunghoon@users.noreply.github.com>
2021-11-23 21:53:04 +0900

    에러메시지 수정

--- backend/categoriesBar.js
+++ backend/categoriesBar.js
@@ -1,33 +1,30 @@
 const express = require("express");
-const model = require('./model')
+const model = require("./model");
 
 const categoriesRouter = express.Router();
 
-
-
-categoriesRouter.get("/", (req, res) => {  
-  let query = 'select * from categories;'
-  model.query(query,function(err,result){
-    if(err){
-      console.log('categoriesBar.js 에 13번째 줄 에러');
-      return err;
+categoriesRouter.get("/", (req, res) => {
+  let query = "select * from categories;";
+  model.query(query, function (err, result) {
+    if (err) {
+      console.log("categoriesBar.js 에 13번째 줄 에러");
+      return res.json({ error: "에러이이" });
     }
-    return result
-  })
+    return result;
+  });
 });
 
-categoriesRouter.get('/subCategories',(req,res)=>{
+categoriesRouter.get("/subCategories", (req, res) => {
   let subCategory = res.query.subCategory;
-  let query = 'select * from sub_categories where parent_category_id='+subCategory;
-  model.query(query,function(err,result){
-    if(err){
+  let query =
+    "select * from sub_categories where parent_category_id=" + subCategory;
+  model.query(query, function (err, result) {
+    if (err) {
       console.log("categoriesBar.js 24번 에러");
-      return err
+      return res.json({ error: "에러이이" });
     }
-    return result
-  })
-})
-
-
+    return result;
+  });
+});
 
 module.exports = categoriesRouter;
--- backend/index.js
+++ backend/index.js
@@ -1,47 +1,41 @@
 const express = require("express");
-const model = require('./model')
+const model = require("./model");
 const cors = require("cors");
 
 const categoryRoute = require("./categoriesBar");
 const mainRoute = require("./main");
-const mediaActorsRoute = require('./mediaActors')
-const theaterActorsRoute = require('./theaterActors')
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
 
-app.use('/',mainRoute)
+app.use("/", mainRoute);
 
 app.use("/categoriesBar", categoryRoute);
 
-app.use("/categories/mediaActors", mediaActorsRoute);
-
-app.use('/categories/theaterActors',theaterActorsRoute);
-
-app.get('/posts', function(req, res) {
+app.get("/posts", function (req, res) {
   let writer = req.query.writer;
 
   if (!writer) {
-    return res.json({'error' : 'writer NOT FOUND'});
+    return res.json({ error: "writer NOT FOUND" });
   }
 
   let query = 'SELECT * FROM board WHERE writer = "' + writer + '"';
   console.log(query);
-  model.query(query, function(err, result) {
+  model.query(query, function (err, result) {
     if (err) {
       console.log(err);
-      return res.json({'error' : 'MYSQL ERROR'});
+      return res.json({ error: "MYSQL ERROR" });
     }
     return res.json(result);
-  })
+  });
 });
 
 app.listen(port, () => {
   console.log(`example app listening at http://localhost:${port}`);
 });
 
-//지금은 index에서 카테고리를 받으면 카테고리 파일로 넘겨준다음 결과값을 받아오는 형식 
-//카테고리 안에 있는 매체연기,연극연기,뮤지컬,무용 카테고리들 안데 또 영상,오디션정보,커뮤니티로 나누워져있다면 
-// 
+//지금은 index에서 카테고리를 받으면 카테고리 파일로 넘겨준다음 결과값을 받아오는 형식
+//카테고리 안에 있는 매체연기,연극연기,뮤지컬,무용 카테고리들 안데 또 영상,오디션정보,커뮤니티로 나누워져있다면
+//
