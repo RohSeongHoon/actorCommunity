0f26a48eee0da9dd0e6c1743ce35347394b544f6
TTeongHoon <87319344+DDeunghoon@users.noreply.github.com>
2021-11-25 22:01:42 +0900

    오류 수정중

--- backend/categoryBar.js
+++ backend/categoryBar.js
@@ -5,22 +5,28 @@ const categoryRouter = express.Router();
 
 categoryRouter.get("/", (req, res) => {
   let query = "select * from categories;";
-  model.query(query, function (err, result) {
-    if (err) {
-      console.log("categoryBar.js 에 13번째 줄 에러");
-      return res.json({ error: "에러이이" });
-    }
-    return res.json(result);
-  });
-});
 
-categoryRouter.get("/subCategories", (req, res) => {
-  let subCategory = res.query.subCategory;
-  let query = "select * from sub_categories where categoy_id=" + subCategory;
   model.query(query, function (err, result) {
     if (err) {
-      console.log("categoriesBar.js 24번 에러");
-      return res.json({ error: "에러이이" });
+      console.log(err);
+      return res.err;
+    }
+  });
+  model.query(query, function (err, result) {
+    if (err) {
+      console.log(err);
+      return res.err;
+    }
+    for (let i = 1; i < result.length; i++) {
+      let getSubCategories =
+        "select * from sub_categories where parent_category_id=" + result[i].id;
+      model.query(getSubCategories, function (err, subCategories) {
+        if (err) {
+          console.log(err);
+          return res.err;
+        }
+        result[i].(subCategories);
+      });
     }
     return res.json(result);
   });
