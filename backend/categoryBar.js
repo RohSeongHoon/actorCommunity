9a033af073842662710ca5bf9cfed25506f9cde3
TTeongHoon <87319344+DDeunghoon@users.noreply.github.com>
2021-11-29 18:22:13 +0900

    db수정 블루버드모듈 다운 카테고리바수정

--- backend/categoryBar.js
+++ backend/categoryBar.js
@@ -3,38 +3,12 @@ const model = require("./model");
 
 const categoryRouter = express.Router();
 
-categoryRouter.get("/", (req, res) => {
-  let query = "select * from categories;";
-
-  model.query(query, async function (err, category) {
-    if (err) {
-      console.log(err);
-      return res.err;
-    }
-    try {
-      let result = await getSubCategories(category);
-      console.log(result);
-      return res.json(result);
-    } catch {
-      console.log(err);
-      return err;
-    }
-  });
+categoryRouter.get("/", async (req, res) => {
+  const [categories] = await model.query("select * from categories;");
+  const [subCategories] = await model.query("select * from sub_Categories");
+  console.log(categories);
+  console.log(subCategories);
+  return res.json({ categories, subCategories });
 });
-let getSubCategories = (category) => {
-  return new Promise((resolve) => {
-    category.forEach((element) => {
-      let getSubCategories =
-        "select * from sub_categories where  parent_category_id=" + element.id;
-      model.query(getSubCategories, function (err, subCategories) {
-        if (err) {
-          console.log(err);
-          return res.err;
-        }
-        element.subCategories = subCategories;
-        return element;
-      });
-    });
-  });
-};
+
 module.exports = categoryRouter;
--- backend/model.js
+++ backend/model.js
@@ -1,19 +1,16 @@
-const mysql = require("mysql2/promise");
-//만들던지 다운 받아야함  npm
+const mysql = require("mysql2");
+const Promise = require("bluebird");
 
-let connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "qsc20215",
   database: "actor_community",
 });
+let poolPromise = pool.promise();
 
-connection.connect(function (err) {
-  if (err) throw err;
-  console.log("connection success");
-});
+module.exports = poolPromise;
 
-module.exports = connection;
 //데이터베이스 테이블 생성시에 _가 2번이상 들어가면 좋지않다고 하는데
 //매체연기 카테고리 안에 영상 , 오디션정보 , 자유게시판 이런식으로 소규모가 있다면 어떤식으로 테이블명을 명세해야하는지 모르겠음
 //그리고 만약 게시물 안에 댓글들이 리스트라면 게시물 테이블에서 댓글 테이블을 참조하는 것으로 생성하면 되는지
