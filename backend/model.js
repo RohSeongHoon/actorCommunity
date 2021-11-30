d156e8545b562ec2999f2ed790ce0c08e4ad7884
TTeongHoon <87319344+DDeunghoon@users.noreply.github.com>
2021-11-30 15:41:27 +0900

    promise 이해안돼는 부분 추가로 공부

--- backend/model.js
+++ backend/model.js
@@ -7,9 +7,15 @@ const pool = mysql.createPool({
   password: "qsc20215",
   database: "actor_community",
 });
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "qsc20215",
+  database: "actor_community",
+});
 let poolPromise = pool.promise();
 
-module.exports = poolPromise;
+module.exports = connection;
 
 //데이터베이스 테이블 생성시에 _가 2번이상 들어가면 좋지않다고 하는데
 //매체연기 카테고리 안에 영상 , 오디션정보 , 자유게시판 이런식으로 소규모가 있다면 어떤식으로 테이블명을 명세해야하는지 모르겠음
