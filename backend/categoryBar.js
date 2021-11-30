718687af5a68b7663fea7373e2fbbb10692a0c76
TTeongHoon <87319344+DDeunghoon@users.noreply.github.com>
2021-11-30 16:51:11 +0900

    프로미스로 수정

--- backend/categoryBar.js
+++ backend/categoryBar.js
@@ -3,12 +3,33 @@ const model = require("./model");
 
 const categoryRouter = express.Router();
 
+// categoryRouter.get("/", async (req, res) => {
+//   const [categories] = await model.query("select * from categories;");
+//   const [subCategories] = await model.query("select * from sub_Categories");
+//   console.log(categories);
+//   console.log(subCategories);
+//   return res.json({ categories, subCategories });
+// });
 categoryRouter.get("/", async (req, res) => {
-  const [categories] = await model.query("select * from categories;");
-  const [subCategories] = await model.query("select * from sub_Categories");
-  console.log(categories);
-  console.log(subCategories);
-  return res.json({ categories, subCategories });
+  const query = "select * from categories";
+  try {
+    let [categories] = await model.query(query);
+    return res.json(categories);
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+});
+
+categoryRouter.get("/subcategories", async (req, res) => {
+  const query = "select * from sub_cateogires";
+  try {
+    let [subCategories] = await model.query(query);
+    return res.json(subCategories);
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
 });
 
 module.exports = categoryRouter;
--- backend/model.js
+++ backend/model.js
@@ -1,21 +1,23 @@
-const mysql = require("mysql2");
-const Promise = require("bluebird");
+// const mysql = require("mysql2/promise");
+// const Promise = require("bluebird");
 
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "qsc20215",
+//   database: "actor_community",
+// });
+// let poolPromise = pool.promise();
+//블루버드 모듈사용 하지만 promise()함수가 이해안되서 새로 짬
+const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "qsc20215",
   database: "actor_community",
 });
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "qsc20215",
-  database: "actor_community",
-});
-let poolPromise = pool.promise();
 
-module.exports = connection;
+module.exports = pool;
 
 //데이터베이스 테이블 생성시에 _가 2번이상 들어가면 좋지않다고 하는데
 //매체연기 카테고리 안에 영상 , 오디션정보 , 자유게시판 이런식으로 소규모가 있다면 어떤식으로 테이블명을 명세해야하는지 모르겠음
