5b8faab43bee8f9ef7345635f8000a15d2ff398b
TTeongHoon <87319344+DDeunghoon@users.noreply.github.com>
2021-12-03 22:45:49 +0900

    착각한것들과 수정해야할것들 주석으로 추가

--- backend/main.js
+++ backend/main.js
@@ -26,7 +26,10 @@ mainRouter.get("/post", async (req, res) => {
           " and sub_category_id = (select id from sub_categories where name_kr = '자유게시판'and parent_category_id = " +
           categoryId +
           " )"
-      );
+      ); // 문법에 맞게 사용 물음표와 [] 사용해서 변수로 삽입
+      //포스트는 포스트.js안에만 있어야함
+      //화면에 맞춰서 api생성하면 효율이 많이 떨어짐
+      //객체 지향인걸 잊으면 안됨
       categories[i].contents = postContents;
     }
     return res.json(categories);
--- backend/post.js
+++ backend/post.js
@@ -1,32 +1,6 @@
-const express = require('express');
-const model = require('./main');
+const express = require("express");
+const model = require("./main");
 
 const postRouter = express.Router();
-
-
-postRouter.get('http://localhost:3000/media/freePost/:number',(req,res)=>{
-    let number = param.req.number;
-    let query = 
-    'select category_number,title,writer,content,write_date from board where board_id='+number;
-    model.query(query,function(err,result){
-        if(err){
-            console.log(err);
-            return err;
-        }
-        res.json(result)
-    })
-})
-postRouter.get('/freePost/:number',(req,res)=>{
-    let number = param.req.number;
-    let query = 
-    'SELECT * FROM comment where board =' + number ;
-    model.query(query,function(err,result){
-        if(err){
-            console.log(err);
-            return err;
-        }
-        res.json(result)
-    })
-})
 
 module.exports = postRouter;
