3e12141516bd7e092cc9f1a7b8042945050a46d9
TTeongHoon <87319344+DDeunghoon@users.noreply.github.com>
2021-12-04 16:16:28 +0900

    main.js 포스트 제거

--- backend/main.js
+++ backend/main.js
@@ -14,30 +14,7 @@ mainRouter.get("/videos", async (req, res) => {
   }
 });
 
-mainRouter.get("/post", async (req, res) => {
-  let query = "select * from categories";
-  try {
-    let [categories] = await model.query(query);
-    for (let i = 0; i < categories.length; i++) {
-      let categoryId = categories[i].id;
-      let [postContents] = await model.query(
-        "select * from posts where category_id =" +
-          categoryId +
-          " and sub_category_id = (select id from sub_categories where name_kr = '자유게시판'and parent_category_id = " +
-          categoryId +
-          " )"
-      ); // 문법에 맞게 사용 물음표와 [] 사용해서 변수로 삽입
-      //포스트는 포스트.js안에만 있어야함
-      //화면에 맞춰서 api생성하면 효율이 많이 떨어짐
-      //객체 지향인걸 잊으면 안됨
-      categories[i].contents = postContents;
-    }
-    return res.json(categories);
-  } catch (err) {
-    console.log(err);
-    return err;
-  }
-});
+//mainRouter.get("/post", async (req, res) => {});
 
 module.exports = mainRouter;
 
