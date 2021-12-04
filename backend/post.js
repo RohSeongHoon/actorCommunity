7c208b7a564e1faa8cb05a42cd55ad78b94503c9
TTeongHoon <87319344+DDeunghoon@users.noreply.github.com>
2021-12-04 16:31:16 +0900

    쿼리문 변수로 변경

--- backend/post.js
+++ backend/post.js
@@ -10,13 +10,11 @@ postRouter.get("/", async function (req, res) {
     for (let i = 0; i < categories.length; i++) {
       let categoryId = categories[i].id;
       let [postContents] = await model.query(
-        "select * from posts where category_id =" +
-          categoryId +
-          " and sub_category_id = (select id from sub_categories where name_kr = '자유게시판'and parent_category_id = " +
-          categoryId +
-          " )"
-        //조건문을 밖에서 쿼리파람으로 요청
-      ); // 문법에 맞게 사용 물음표와 [] 사용해서 변수로 삽입
+        "select * from posts where category_id =? and sub_category_id = (select id from sub_categories where name_kr = '자유게시판'and parent_category_id = ?)",
+        [categoryId, categoryId]
+      );
+      //조건문을 밖에서 쿼리파람으로 요청
+      // 문법에 맞게 사용 물음표와 [] 사용해서 변수로 삽입
       //포스트는 포스트.js안에만 있어야함
       //화면에 맞춰서 api생성하면 효율이 많이 떨어짐
       //객체 지향인걸 잊으면 안됨
