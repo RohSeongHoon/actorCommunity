657afafa38aea90538989c332e3ea0ccb57a719d
TTeongHoon <87319344+DDeunghoon@users.noreply.github.com>
2021-12-06 22:15:01 +0900

    api로 데이터를 나눠서 가지고 오지말고 덩어리로 한번에 가지고와야함

--- backend/categories.js
+++ backend/categories.js
@@ -3,23 +3,6 @@ const model = require("./model");
 
 const categoryRouter = express.Router();
 
-// categoryRouter.get("/", async (req, res) => {
-//   const [categories] = await model.query("select * from categories;");
-//   const [subCategories] = await model.query("select * from sub_Categories");
-//   console.log(categories);
-//   console.log(subCategories);
-//   return res.json({ categories, subCategories });
-// });
-// categoryRouter.get("/", async (req, res) => {
-//   const query = "select * from categories";
-//   try {
-//     let [categories] = await model.query(query);
-//     return res.json(categories);
-//   } catch (err) {
-//     console.log(err);
-//     return err;
-//   }
-// });
 categoryRouter.get("/", async (req, res) => {
   const query = "select * from categories";
   try {
@@ -37,9 +20,30 @@ categoryRouter.get("/", async (req, res) => {
     return err;
   }
 });
+categoryRouter.get("/list/freePost", async (req, res) => {});
+categoryRouter.get("/list/videos");
+
+module.exports = categoryRouter;
+
 //1. 카테고리와 하위 카테고리를 같이 받아서 온다
 //2. 카테고리를 받아온다
 //3. for문을 돌면서 카테고리 아이디가 1인 하위카테고리를 아이디가 1인 카테고리에 추가한다
 //4. 값을 리턴한다
 
-module.exports = categoryRouter;
+// categoryRouter.get("/", async (req, res) => {
+//   const [categories] = await model.query("select * from categories;");
+//   const [subCategories] = await model.query("select * from sub_Categories");
+//   console.log(categories);
+//   console.log(subCategories);
+//   return res.json({ categories, subCategories });
+// });
+// categoryRouter.get("/", async (req, res) => {
+//   const query = "select * from categories";
+//   try {
+//     let [categories] = await model.query(query);
+//     return res.json(categories);
+//   } catch (err) {
+//     console.log(err);
+//     return err;
+//   }
+// });
--- backend/post.js
+++ backend/post.js
@@ -3,23 +3,23 @@ const model = require("./model");
 
 const postRouter = express.Router();
 
-postRouter.get("/", async function (req, res) {
+postRouter.get("/mainFreePost", async function (req, res) {
   let subCategoryId = req.query.subCategoryId;
   try {
     let [postContents] = await model.query(
       "select * from posts where sub_category_id =" + subCategoryId
     );
-    //조건문을 밖에서 쿼리파람으로 요청
-    // 문법에 맞게 사용 물음표와 [] 사용해서 변수로 삽입
-    //포스트는 포스트.js안에만 있어야함
-    //화면에 맞춰서 api생성하면 효율이 많이 떨어짐
-    //객체 지향인걸 잊으면 안됨
-
     return res.json(postContents);
   } catch (err) {
     console.log(err);
     return err;
   }
 });
+//조건문을 밖에서 쿼리파람으로 요청
+// 문법에 맞게 사용 물음표와 [] 사용해서 변수로 삽입
+//포스트는 포스트.js안에만 있어야함
+//화면에 맞춰서 api생성하면 효율이 많이 떨어짐
+//객체 지향인걸 잊으면 안됨
 
+postRouter.get("/post"), async function (req, res) {};
 module.exports = postRouter;
