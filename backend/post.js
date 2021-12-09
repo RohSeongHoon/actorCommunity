efd91b112a32094ab7c9c43ea55e8e836d8d2a08
TTeongHoon <87319344+DDeunghoon@users.noreply.github.com>
2021-12-09 19:50:55 +0900

    video-community 생성 및 기본틀 생성

--- backend/post.js
+++ backend/post.js
@@ -70,6 +70,11 @@ postRouter.get("/list", async (req, res) => {
 
   // let [commentCount] = await model.query("select count(*) from comments");
 });
+postRouter.get("/videoList", async (req, res) => {
+  let categoryId = req.query.categoryId;
+  let subCategoryId = req.query.subCategoryId;
+  console.log("post.js:76", categoryId, subCategoryId);
+});
 //조건문을 밖에서 쿼리파람으로 요청
 // 문법에 맞게 사용 물음표와 [] 사용해서 변수로 삽입
 //포스트는 포스트.js안에만 있어야함
