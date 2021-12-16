8dedfd8957f1fc772e7d356486f030329f1ccd49
TTeongHoon <87319344+DDeunghoon@users.noreply.github.com>
2021-12-16 15:18:39 +0900

    post형식은 /로 마무리한다

--- backend/posts.js
+++ backend/posts.js
@@ -68,7 +68,7 @@ postRouter.get("/list", async (req, res) => {
 
   // let [commentCount] = await model.query("select count(*) from comments");
 });
-postRouter.post("/add", async (req, res) => {
+postRouter.post("/", async (req, res) => {
   const categoryId = req.query.categoryId;
   const subCategoryId = req.query.subCategoryId;
   const title = req.body.title;
@@ -82,6 +82,8 @@ postRouter.post("/add", async (req, res) => {
     return err;
   }
 });
+//body
+
 //조건문을 밖에서 쿼리파람으로 요청
 // 문법에 맞게 사용 물음표와 [] 사용해서 변수로 삽입
 //포스트는 포스트.js안에만 있어야함
